0721ef2c2850d857e31f8b9eb0581743a3778024
Jorge Fuentes <jorge@jorgefuentes.net>
2020-04-19 18:47:56 +0200

    Update zsh config

--- src/config/zsh/bindings.sh
+++ src/config/zsh/bindings.sh
@@ -1,3 +1,43 @@
 # vim:ft=zsh
 bindkey -v
+
+# If NumLock is off, translate keys to make them appear the same as with NumLock on.
+bindkey -s '^[OM' '^M'  # enter
+bindkey -s '^[Ok' '+'
+bindkey -s '^[Om' '-'
+bindkey -s '^[Oj' '*'
+bindkey -s '^[Oo' '/'
+bindkey -s '^[OX' '='
+
+# If someone switches our terminal to application mode (smkx), translate keys to make
+# them appear the same as in raw mode (rmkx).
+bindkey -s '^[OH' '^[[H'  # home
+bindkey -s '^[OF' '^[[F'  # end
+bindkey -s '^[OA' '^[[A'  # up
+bindkey -s '^[OB' '^[[B'  # down
+bindkey -s '^[OD' '^[[D'  # left
+bindkey -s '^[OC' '^[[C'  # right
+
+# TTY sends different key codes. Translate them to regular.
+bindkey -s '^[[1~' '^[[H'  # home
+bindkey -s '^[[4~' '^[[F'  # end
+
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '^?' backward-delete-char
+bindkey '^[[3~' delete-char
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
+bindkey '^H' backward-kill-word
+bindkey '^[[3;5~' kill-word
+bindkey '^J' backward-kill-line
+bindkey '^[[D' backward-char
+bindkey '^[[C' forward-char
+bindkey '^[[A' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search
+
 bindkey '^R' history-incremental-search-backward
--- src/config/zsh/environment.sh
+++ src/config/zsh/environment.sh
@@ -1,14 +1,14 @@
-# Editor
+# editor
 export EDITOR=vim
 export VISUAL=vim
 
-# Use neovim if available
+# use neovim if available
 if type nvim > /dev/null; then
   export EDITOR=nvim
   alias vim=nvim
 fi
 
-# Language
+# language
 export LC_ALL=en_US.UTF-8
 export LC_COLLATE=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
@@ -19,5 +19,5 @@ export LC_TIME=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-# Terminal
+# terminal
 [[ $TMUX = "" ]] && export TERM="xterm-256color"
